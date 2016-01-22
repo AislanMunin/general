710bc30ace89d3bcc2b9dfb2748eb01cf7e5e6da
Rafael 'fumasa' Giorgetti <rafael@fumasa.org>
2016-01-22 10:20:30 +0000

    fixing raven

--- hooker/index.js
+++ hooker/index.js
@@ -1,7 +1,8 @@
+var hookshot = require('hookshot');
+var raven = require('raven');
+
 var client = new raven.Client('https://0c2b0522792e419896a33f821b5aa144:93ea68f01a6848c0be3ac522c07cf4ac@app.getsentry.com/64613');
 client.patchGlobal();
-
-var hookshot = require('hookshot');
 
 hookshot()
 .on('refs/heads/master', 'git pull')
