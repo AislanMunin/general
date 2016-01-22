83aa24ed83fc90be0924940ec103407ed70d8022
Rafael 'fumasa' Giorgetti <rafael@fumasa.org>
2016-01-22 10:03:14 +0000

    adding raven/getsentry

--- hooker/index.js
+++ hooker/index.js
@@ -1,3 +1,6 @@
+var client = new raven.Client('https://0c2b0522792e419896a33f821b5aa144:93ea68f01a6848c0be3ac522c07cf4ac@app.getsentry.com/64613');
+client.patchGlobal();
+
 var hookshot = require('hookshot');
 
 hookshot()
